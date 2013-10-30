b84e403bb3c6bb69f2dd38e72f68be29488177ca
adesousa <andre.desousa.95@gmail.com>
2013-10-30 18:59:32 +0100

    Adding the compiz export profile file, changes in install.sh and preparing sublime folder for its conf

---
+++ dot-files/compiz-settings-export.profile
@@ -0,0 +1,80 @@
+[place]
+s0_mode = 1
+
+[gnomecompat]
+s0_run_command_screenshot_key = Disabled
+s0_command_window_screenshot = gnome-screenshot -w
+s0_run_command_window_screenshot_key = Disabled
+s0_command_terminal = x-terminal-emulator
+s0_run_command_terminal_key = <Control><Alt>t
+
+[move]
+s0_initiate_key = Disabled
+
+[decor]
+s0_active_shadow_color = #00000080
+s0_inactive_shadow_color = #000000ff
+
+[wall]
+s0_thumb_highlight_gradient_shadow_color = #dfdfdfff
+s0_arrow_base_color = #e6e6e6d9
+s0_arrow_shadow_color = #dcdcdcd9
+
+[animation]
+s0_open_random_effects = animation:Curved Fold;animation:Horizontal Folds;animation:Dream;animation:Fade;animation:Glide 1;animation:Glide 2;animation:Magic Lamp;animation:Magic Lamp Wavy;animation:Sidekick;animation:Wave;animation:Zoom;
+s0_close_random_effects = animation:Curved Fold;animation:Horizontal Folds;animation:Dream;animation:Fade;animation:Glide 1;animation:Glide 2;animation:Magic Lamp;animation:Magic Lamp Wavy;animation:Sidekick;animation:Wave;animation:Zoom;
+s0_minimize_random_effects = animation:Curved Fold;animation:Horizontal Folds;animation:Dream;animation:Fade;animation:Glide 1;animation:Glide 2;animation:Magic Lamp;animation:Magic Lamp Wavy;animation:Sidekick;animation:Zoom;
+s0_unminimize_effects = animation:Glide 2;
+s0_unminimize_random_effects = animation:Curved Fold;animation:Horizontal Folds;animation:Dream;animation:Fade;animation:Glide 1;animation:Glide 2;animation:Magic Lamp;animation:Magic Lamp Wavy;animation:Sidekick;animation:Zoom;
+
+[ezoom]
+s0_zoom_box_outline_color = #2f2f4f9f
+s0_zoom_box_fill_color = #2f2f2f4f
+
+[scale]
+s0_overlay_icon = 1
+s0_initiate_all_edge = TopLeft
+s0_initiate_all_key = <Shift><Super>w
+s0_show_desktop = true
+
+[unityshell]
+s0_show_hud = Disabled
+s0_execute_command = <Super>r
+s0_show_desktop_key = <Super>d
+s0_panel_first_menu = <Super>f
+s0_window_right_maximize = Disabled
+s0_window_left_maximize = Disabled
+s0_panel_opacity = 0,957500
+s0_show_launcher = <Super>a
+s0_keyboard_focus = Disabled
+s0_launcher_switcher_forward = Disabled
+s0_launcher_switcher_prev = Disabled
+s0_launcher_hide_mode = 1
+s0_autohide_animation = 0
+s0_edge_responsiveness = 5,228571
+s0_backlight_mode = 4
+s0_menus_fadein = 0
+s0_menus_discovery_duration = 3
+s0_alt_tab_next_window = <Super>Tab
+s0_alt_tab_prev_window = <Control><Super>Tab
+s0_disable_show_desktop = true
+
+[expo]
+s0_expo_edge = BottomLeft
+s0_x_offset = 0
+s0_y_offset = 0
+s0_distance = 0,005000
+s0_vp_saturation = 40,000000
+s0_selected_color = #cc0000ff
+s0_ground_color1 = #b3b3b3cc
+s0_ground_color2 = #b3b3b300
+
+[core]
+s0_active_plugins = core;composite;opengl;compiztoolbox;copytex;decor;wall;imgpng;vpswitch;mousepoll;regex;commands;resize;unitymtgrabhandles;grid;place;gnomecompat;move;snap;animation;session;expo;fade;workarounds;ezoom;scale;unityshell;
+s0_edge_delay = 112
+s0_autoraise = false
+s0_show_desktop_key = <Super>d
+s0_show_desktop_edge = BottomRight
+s0_hsize = 4
+s0_number_of_desktops = 4
+
--- install.sh
+++ install.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
-#Version v1.2.4
+#Version v1.3.1
 #Script d’installation d’un nouvel environnement Debian-based (install.sh)
 #Exécuter ce script en root, sinon cela ne fonctionne pas.
 #Pour plus d'optimisations système lire ici : http://doc.ubuntu-fr.org/optimisation
 #Pour une meilleure configuration d'un serveur web lire ici : http://blog.nas-admin.org/?p=25
 
-echo "# Création d'un fichier de log pour tracer les opération d'installation" > ./log_install_dd.txt
+# On se place sur le dossier de dev pour effectuer ces commandes
+cd /Data/Linux/2-Dev/
+
+echo "# Création d'un fichier de log pour tracer les opérations d'installation" > ./log_install_dd.txt
+
+read -p "Avant de continuer l'installation, récuperer la clé SSH pour github : suivre la procédure de keygen-ssh de github"
+echo "Install git + git clone de mon repo adesousa"
+sudo apt-get install -qq -y git
+git clone git@github.com:adesousa/conf-files.git conf-files
+
+echo "# Installation de VIM" | tee -a ./log_install_dd.txt
+sudo apt-get install -qq -y vim | tee -a ./log_install_dd.txt
 
 echo "# Mise à jour des paquets du nouvel OS installé (apt-get update + upgrade)" | tee -a ./log_install_dd.txt
 sudo apt-get update -qq | tee -a ./log_install_dd.txt
 sudo apt-get upgrade -qq | tee -a ./log_install_dd.txt
+
 
 echo "# Installation des services nécessaires" | tee -a ./log_install_dd.txt
 echo "# Installation de Nginx: Linux Nginx Mysql Php (LNMP) + PHP5-fpm" | tee -a ./log_install_dd.txt
@@ -23,22 +35,28 @@ sudo mkdir /var/www/errors/global/ | tee -a ./log_install_dd.txt
 sudo cp /var/run/nginx/www/50x.html /var/www/errors/global/50x.html | tee -a ./log_install_dd.txt
 sudo /etc/init.d/nginx reload | tee -a ./log_install_dd.txt
 
+
 echo "# Installation de Mysql et de son extension php" | tee -a ./log_install_dd.txt
 sudo apt-get install mysql-server php5-mysql -qq -y | tee -a ./log_install_dd.txt
+
 
 echo "# Installation des extensions php nécessaires au développement php" | tee -a ./log_install_dd.txt
 sudo apt-get install -qq -y php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl | tee -a ./log_install_dd.txt
 
-echo "# Installation de APC et Varnish" | tee -a ./log_install_dd.txt
-sudo apt-get install php-apc varnish -qq -y | tee -a ./log_install_dd.txt
+
+echo "# Installation de APC" | tee -a ./log_install_dd.txt
+sudo apt-get install php-apc -qq -y | tee -a ./log_install_dd.txt
 echo 'apc.shm_size=100' >> /etc/php5/fpm/conf.d/apc.ini
+
 
 echo "# Installation de Memcached et de son extension php5" | tee -a ./log_install_dd.txt
 sudo apt-get install memcached php5-memcache -qq -y | tee -a ./log_install_dd.txt
 
+
 echo "# Installation de capifony" | tee -a ./log_install_dd.txt
 apt-get install rubygems -qq -y | tee -a ./log_install_dd.txt
 gem install capifony | tee -a ./log_install_dd.txt
+
 
 echo "# Installation et reconfiguration de phpmyadmin"  | tee -a ./log_install_dd.txt
 sudo mkdir /var/www | tee -a ./log_install_dd.txt
@@ -46,8 +64,10 @@ sudo apt-get install -qq -y phpmyadmin | tee -a ./log_install_dd.txt
 sudo ln -s /usr/share/phpmyadmin /var/www/phpmyadmin | tee -a ./log_install_dd.txt
 sed -i -e "s/\/\/ \$cfg\['Servers'\]\[\$i\]\['AllowNoPassword'\]/\$cfg\['Servers'\]\[\$i\]\['AllowNoPassword'\]/g" /etc/phpmyadmin/config.inc.php | tee -a ./log_install_dd.txt
 
+
 echo "# On crée un fichier phpinfo() pour vérifier l'installation" | tee -a ./log_install_dd.txt
 echo "<?php phpinfo(); ?>" > /var/www/phpinfo.php
+
 
 echo "# Création d’un script de lancement des services mysql + Nginx au même endroit que le script d'installation" | tee -a ./log_install_dd.txt
 sudo touch ./start_server_web.sh | tee -a ./log_install_dd.txt
@@ -58,9 +78,11 @@ sudo /etc/init.d/php5-fpm start
 " >> ./start_server_web.sh
 sudo chmod 777 ./start_server_web.sh | tee -a ./log_install_dd.txt
 
+
 echo "# Installation de curl et de composer.phar" | tee -a ./log_install_dd.txt
 sudo apt-get install -qq -y curl | tee -a ./log_install_dd.txt
 sudo curl -s http://getcomposer.org/installer | php -- --install-dir=/usr/bin
+
 
 echo "# Installation de nodeJs et less" | tee -a ./log_install_dd.txt
 sudo apt-get install python-software-properties -qq -y | tee -a ./log_install_dd.txt
@@ -69,69 +91,55 @@ sudo apt-get update -qq > /dev/null
 sudo apt-get install -qq -y nodejs npm | tee -a ./log_install_dd.txt
 sudo npm install -g less | tee -a ./log_install_dd.txt
 
+
 echo "# Configuration de git" | tee -a ./log_install_dd.txt
 sudo cp conf-files/git/.git* /root/ | tee -a ./log_install_dd.txt
 sudo cp conf-files/git/.git* /home/adesousa/ | tee -a ./log_install_dd.txt
 
+
 echo "################# Installations diverses ################" | tee -a ./log_install_dd.txt
-echo "# Installation de Google Chrome" | tee -a ./log_install_dd.txt
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - | tee -a ./log_install_dd.txt
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' | tee -a ./log_install_dd.txt
-sudo apt-get update -qq > /dev/null
-sudo apt-get install -qq -y google-chrome-stable | tee -a ./log_install_dd.txt
 
-echo "# Installation de Spotify" | tee -a ./log_install_dd.txt
-echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4E9CFF4E
-sudo apt-get update -qq > /dev/null
-sudo apt-get install -qq -y spotify-client-qt | tee -a ./log_install_dd.txt
+#echo "# Installation de Spotify" | tee -a ./log_install_dd.txt
+#echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+#sudo apt-get update -qq > /dev/null
+#sudo apt-get install -qq -y spotify-client | tee -a ./log_install_dd.txt
 
-echo "# Installation de rhytmbox" | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y rhythmbox | tee -a ./log_install_dd.txt
-
-echo "# Installation de Netbeans pour php - Méthode via l'installeur depuis leur site qui fonctionne mieux que l'apt-get..." | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y openjdk-7-jre | tee -a ./log_install_dd.txt
-wget http://bits.netbeans.org/netbeans/7.1/community/latest/bundles/netbeans-7.1-ml-php-linux.sh | tee -a ./log_install_dd.txt
-sh netbeans-7.1-ml-php-linux.sh  | tee -a ./log_install_dd.txt
-sudo cp -r conf-files/netbeans/ ./ | tee -a ./log_install_dd.txt
 
 echo "Copie de la doc symfony2 compilée avec Sphynx" | tee -a ./log_install_dd.txt
 sudo cp -r conf-files/symfony/ ./ | tee -a ./log_install_dd.txt
+
 
 echo "Configuration des .dot-files pour bash" | tee -a ./log_install_dd.txt
 sudo cp conf-files/dot-files/.bash_aliases /root/ | tee -a ./log_install_dd.txt
 sudo cp conf-files/dot-files/.bash_aliases /home/adesousa/ | tee -a ./log_install_dd.txt
 
-echo "# Installation de Jupiter pour la gestion de l’alimentation d’un pc portable -> ATTENTION, il n'est peut-être pas si efficace que cela si l'on utilise bien powertop !" | tee -a ./log_install_dd.txt
-sudo add-apt-repository -y ppa:webupd8team/jupiter | tee -a ./log_install_dd.txt
-sudo apt-get update -qq > /dev/null
-sudo apt-get install -qq -y jupiter | tee -a ./log_install_dd.txt
-
-echo "# Installation de VIM" | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y vim | tee -a ./log_install_dd.txt
-
-echo "# Installation de subversion" | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y subversion | tee -a ./log_install_dd.txt
 
 echo "# Installation de preload pour l'amélioration des performances" | tee -a ./log_install_dd.txt
 sudo apt-get install -qq -y preload | tee -a ./log_install_dd.txt
 
-echo "# Installation de powertop pour améliorer la batterie (lancer powertop > onglet tunab et tout passer à GOOD + commande powertop --calibrate)" | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y powertop | tee -a ./log_install_dd.txt
 
-echo "# Installation de laptop-mode-tools pour l'amélioration des performances sur un portable" | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y laptop-mode-tools | tee -a ./log_install_dd.txt
+echo "# Forcer l’utilisation de la mémoire vive au dépend du swap" | tee -a ./log_install_dd.txt
+sudo echo "vm.swappiness = 10" >> /etc/sysctl.conf | tee -a ./log_install_dd.txt
 
-echo "# Installation de ubuntu-tweak, pour gérer les paramètres d'ubuntu" | tee -a ./log_install_dd.txt
-sudo add-apt-repository -y ppa:tualatrix/next | tee -a ./log_install_dd.txt
+
+echo "# Désactiver apport (service de remontée de bugs)" | tee -a ./log_install_dd.txt
+sudo sed -i -e s/enabled=1/enabled=0/g /etc/default/apport
+
+
+echo "# Installation de Compiz Config Settings Manager, Unity tweak tools et ubuntu-tweak, pour gérer les paramètres d'ubuntu" | tee -a ./log_install_dd.txt
+sudo apt-get install compizconfig-settings-manager unity-tweak-tool -qq -y | tee -a ./log_install_dd.txt
+sudo add-apt-repository -y ppa:tualatrix/ppa | tee -a ./log_install_dd.txt
 sudo apt-get update -qq > /dev/null
 sudo apt-get install ubuntu-tweak -qq | tee -a ./log_install_dd.txt
+
 
 echo "# Désinstallation de quelques services ou logiciels non nécessaires" | tee -a ./log_install_dd.txt
 echo "# Remove Apache2 et Lighttpd pour éviter les conflits avec les différents serveurs web existants sur le poste" | tee -a ./log_install_dd.txt
 sudo apt-get remove -qq -y --purge apache2 | tee -a ./log_install_dd.txt
 sudo apt-get remove -qq -y --purge lighttpd | tee -a ./log_install_dd.txt
 sudo rm -r /etc/init.d/apache2 | tee -a ./log_install_dd.txt
+
 
 echo "# Suppression des services mysql,Nginx, php5, rsync, bluetooth et Apache2 (qui vient d'être supprimé) au démarrage" | tee -a ./log_install_dd.txt
 sudo update-rc.d -f mysql remove | tee -a ./log_install_dd.txt
@@ -144,47 +152,17 @@ sudo update-rc.d -f pppd-dns remove | tee -a ./log_install_dd.txt
 sudo update-rc.d -f pcmciautils remove | tee -a ./log_install_dd.txt
 # sudo update-rc.d -f speech-dispatcher remove | tee -a ./log_install_dd.txt : A tester avant de supprimer du démarrage...
 
+
 echo "# Chown de /var/www pour que Nginx puisse lire comme il se doit dans les dossiers web et éviter des pb de droits" | tee -a ./log_install_dd.txt
 chown -R www-data:www-data /var/www | tee -a ./log_install_dd.txt
 
-# Installation de phpunit pour Symfony2
-sudo pear upgrade PEAR  | tee -a ./log_install_dd.txt
-pear config-set auto_discover 1 | tee -a ./log_install_dd.txt
-pear install pear.phpunit.de/PHPUnit | tee -a ./log_install_dd.txt
-
-echo "# Installation de Gedit et de quelques plugins" | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y gedit | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y gedit-plugins | tee -a ./log_install_dd.txt
-sudo apt-add-repository ppa:ubuntu-on-rails/ppa
-sudo apt-get update -qq > /dev/null
-sudo apt-get install -qq -y gedit-gmate exuberant-ctags | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y dconf-tools | tee -a ./log_install_dd.txt
-sudo apt-get install python-gi-cairo -qq -y | tee -a ./log_install_dd.txt
-# installation de plusieurs plugins: vigedit, tabSwitcher ...
-sudo cp -r conf-files/gedit/plugins/ /usr/lib/gedit/plugins/
-sudo cp conf-files/gedit/twig.lang /usr/share/gtksourceview-3.0/language-specs/ | tee -a ./log_install_dd.txt
-sudo dconf load /org/gnome/gedit/ < conf-files/gedit/gedit.settings | tee -a ./log_install_dd.txt
-sudo cp -r /root/.config/dconf /home/adesousa/.config/ | tee -a ./log_install_dd.txt
-
-#### S'il y a des changements dans la conf gedit : voici une méthode pour regénerer un dump des settings de gedit
-#sudo dconf dump /org/gnome/gedit/ > gedit.settings
-#sudo dconf load /org/gnome/gedit/ < gedit.settings
-#sudo cp -r /root/.config/dconf /home/adesousa/.config/
-
-echo "# Installation des différents bureaux Mint sous linux ou debian, ya plus qu'à choisir au démarrage celui que je préfère" | tee -a ./log_install_dd.txt
-sudo add-apt-repository ppa:webupd8team/gnome3 | tee -a ./log_install_dd.txt
-sudo add-apt-repository --yes ppa:gwendal-lebihan-dev/cinnamon-stable | tee -a ./log_install_dd.txt
-sudo apt-get update -qq | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y gnome-shell gnome-session gnome-session-fallback gnome-tweak-tool cinnamon muffin | tee -a ./log_install_dd.txt
-# facultatifs mais jolies...
-sudo apt-get install -qq -y cinnamon-extensions-extended-places-menu | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y cinnamon-extension-weather cinnamon-extensions-system-monitor | tee -a ./log_install_dd.txt
-sudo add-apt-repository ppa:webupd8team/gnome3 | tee -a ./log_install_dd.txt
-sudo apt-get update -qq | tee -a ./log_install_dd.txt
-sudo apt-get install -qq -y gnome-shell gnome-session mgse-bottompanel mgse-menu mgse-windowlist gnome-tweak-tool | tee -a ./log_install_dd.txt
 
 echo "# Purger l'ensemble des fichiers de configurations suites aux installations - désinstallations" | tee -a ./log_install_dd.txt
 sudo dpkg --purge $(COLUMNS=200 dpkg -l | grep "^rc" | tr -s ' ' | cut -d ' ' -f 2)  | tee -a ./log_install_dd.txt
+
+echo "Install git + git clone de mon repo adesousa"
+sudo apt-get install -qq -y git
+git clone git@github.com:adesousa/conf-files.git conf-files
 
 read -p "Avant de continuer l'installation, récuperer les clés SSH pour github et assembla qui permettront de se connecter aux différents projets existants"
 echo "# Configuration des projets et autres éléments" | tee -a ./log_install_dd.txt
@@ -202,4 +180,74 @@ cp app/config/parameters-dist.yml parameters.yml
 composer.phar install
 cd /home/adesousa/Bureau
 echo "Penser à récuperer le script amazon et sa clé ssh"
+
+read -p "Penser à importer le fichier de conf compiz notamment pour les raccourcis et comportement des fenêtres"
+
 echo "Installation done: Reboot to enjoy the power!"
+
+#
+# Deprecated car logiciels plus nécessaire dans les nouvelles versions ou je m'en sers plus...
+#
+#
+# echo "# Installation de laptop-mode-tools pour l'amélioration des performances sur un portable" | tee -a ./log_install_dd.txt
+# sudo apt-get install -qq -y laptop-mode-tools | tee -a ./log_install_dd.txt
+#
+#echo "# Installation de powertop pour améliorer la batterie (lancer powertop > onglet tunab et tout passer à GOOD + commande powertop --calibrate)" | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y powertop | tee -a ./log_install_dd.txt
+#
+#echo "# Installation de Gedit et de quelques plugins" | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y gedit | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y gedit-plugins | tee -a ./log_install_dd.txt
+#sudo apt-add-repository -y ppa:ubuntu-on-rails/ppa
+#sudo add-apt-repository ppa:gedit-bc-dev-plugins/releases
+#sudo apt-get update -qq > /dev/null
+#sudo apt-get install -qq -y gedit-gmate exuberant-ctags | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y dconf-tools | tee -a ./log_install_dd.txt
+#sudo apt-get install python-gi-cairo -qq -y | tee -a ./log_install_dd.txt
+#sudo apt-get install gedit-classbrowser3g
+#
+# installation de plusieurs plugins: vigedit, tabSwitcher ...
+#mkdir -p ~/.local/share/gedit/plugins/
+#sudo cp -r conf-files/gedit/plugins/* ~/.local/share/gedit/plugins/
+#sudo cp conf-files/gedit/twig.lang /usr/share/gtksourceview-3.0/language-specs/ | tee -a ./log_install_dd.txt
+#sudo dconf load /org/gnome/gedit/ < conf-files/gedit/gedit.settings | tee -a ./log_install_dd.txt
+#sudo cp -r /root/.config/dconf /home/adesousa/.config/ | tee -a ./log_install_dd.txt
+#
+#### S'il y a des changements dans la conf gedit : voici une méthode pour regénerer un dump des settings de gedit
+#sudo dconf dump /org/gnome/gedit/ > gedit.settings
+#sudo dconf load /org/gnome/gedit/ < gedit.settings
+#sudo cp -r /root/.config/dconf /home/adesousa/.config/
+#
+#echo "# Installation des différents bureaux Mint sous linux ou debian, ya plus qu'à choisir au démarrage celui que je préfère" | tee -a ./log_install_dd.txt
+#sudo add-apt-repository ppa:webupd8team/gnome3 | tee -a ./log_install_dd.txt
+#sudo add-apt-repository --yes ppa:gwendal-lebihan-dev/cinnamon-stable | tee -a ./log_install_dd.txt
+#sudo apt-get update -qq | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y gnome-shell gnome-session gnome-session-fallback gnome-tweak-tool cinnamon muffin | tee -a ./log_install_dd.txt
+# facultatifs mais jolies...
+#sudo apt-get install -qq -y cinnamon-extensions-extended-places-menu | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y cinnamon-extension-weather cinnamon-extensions-system-monitor | tee -a ./log_install_dd.txt
+#sudo add-apt-repository ppa:webupd8team/gnome3 | tee -a ./log_install_dd.txt
+#sudo apt-get update -qq | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y gnome-shell gnome-session mgse-bottompanel mgse-menu mgse-windowlist gnome-tweak-tool | tee -a ./log_install_dd.txt
+#
+# Installation de phpunit pour Symfony2
+#sudo pear upgrade PEAR  | tee -a ./log_install_dd.txt
+#pear config-set auto_discover 1 | tee -a ./log_install_dd.txt
+#pear install pear.phpunit.de/PHPUnit | tee -a ./log_install_dd.txt
+#
+#echo "# Installation de subversion" | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y subversion | tee -a ./log_install_dd.txt
+#
+#echo "# Installation de Google Chrome" | tee -a ./log_install_dd.txt
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - | tee -a ./log_install_dd.txt
+#sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' | tee -a ./log_install_dd.txt
+#sudo apt-get update -qq > /dev/null
+#sudo apt-get install -qq -y google-chrome-stable | tee -a ./log_install_dd.txt
+#echo "# Installation de rhytmbox" | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y rhythmbox | tee -a ./log_install_dd.txt
+#
+#echo "# Installation de Netbeans pour php - Méthode via l'installeur depuis leur site qui fonctionne mieux que l'apt-get..." | tee -a ./log_install_dd.txt
+#sudo apt-get install -qq -y openjdk-7-jre | tee -a ./log_install_dd.txt
+#wget http://bits.netbeans.org/netbeans/7.1/community/latest/bundles/netbeans-7.1-ml-php-linux.sh | tee -a ./log_install_dd.txt
+#sh netbeans-7.1-ml-php-linux.sh  | tee -a ./log_install_dd.txt
+#sudo cp -r conf-files/netbeans/ ./ | tee -a ./log_install_dd.txt
